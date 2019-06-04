60b455e00bbed2ab59371727c570303adf4c93cf
Patrick Lamoureux <patrick@blackcedar.com>
2019-06-04 10:17:02 -0600

    the constructor route and some semantic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { Container, Header, Button } from "semantic-ui-react"
 
 class App extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      contacts: [
+        {id: 1, firstName: "Jerry", phone: "801-234-3243"},
+        {id: 2, firstName: "Tom", phone: "801-745-3263"},
+        {id: 3, firstName: "Marry", phone: "801-339-9734"},
+      ]
+    };
+  }
+  
   render(){
     return(
-      <div>
-        <h1>Hello World</h1>
-      </div>
+      <Container>
+        <Header as ="h1" color = "red">React Contact List</Header>
+        <Button primary>Button</Button>
+      </Container>
     )
   }
   
